25513bb603d867317e147285343807f5f0b6b346
Oliver Forral <intrepion@gmail.com>
2015-03-22 14:42:27 -0700

    Part 2 - Double Jumping & Finding The Ground

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
-
+public class PlayerController : MonoBehaviour
+{
 	public float moveSpeed;
 	public float jumpHeight;
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
 
+	private bool grounded;
+	private bool doubleJumped;
 	private Rigidbody2D rigidbody2D;
 
 	// Use this for initialization
-	void Start () {
-		this.rigidbody2D = GetComponent<Rigidbody2D> (); 
+	void Start ()
+	{
+		this.rigidbody2D = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate()
+	{
+		this.grounded = Physics2D.OverlapCircle (this.groundCheck.position, groundCheckRadius, whatIsGround);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			this.rigidbody2D.velocity = new Vector2(this.rigidbody2D.velocity.x, jumpHeight);
+		if (grounded) {
+			this.doubleJumped = false;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space) && this.grounded) {
+			this.Jump ();
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space) && !this.doubleJumped && !this.grounded) {
+			this.Jump ();
+			this.doubleJumped = true;
 		}
 
 		if (Input.GetKey (KeyCode.RightArrow)) {
@@ -28,5 +49,10 @@ public class PlayerController : MonoBehaviour {
 			this.rigidbody2D.velocity = new Vector2(-moveSpeed, this.rigidbody2D.velocity.y);
 		}
 		
+	}
+
+	private void Jump()
+	{
+		this.rigidbody2D.velocity = new Vector2(this.rigidbody2D.velocity.x, jumpHeight);
 	}
 }
