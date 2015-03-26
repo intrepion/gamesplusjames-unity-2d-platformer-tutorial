551ca2548817e31ee9f816b3ffc217de26b6d3c3
Oliver Forral <intrepion@gmail.com>
2015-03-25 23:52:00 -0700

    Part 13 - Changing Levels & Level Select

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour
+{
+	public string levelToLoad;
+
+	private bool playerInZone;
+
+	// Use this for initialization
+	void Start ()
+	{
+		playerInZone = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.UpArrow) && this.playerInZone) {
+			Application.LoadLevel (this.levelToLoad);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.name == "Player") {
+			this.playerInZone = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.name == "Player") {
+			this.playerInZone = false;
+		}
+	}
+}
