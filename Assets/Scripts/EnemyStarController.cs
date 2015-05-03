f6835a813447fa4334d5283839743f669b533356
Oliver Forral <intrepion@gmail.com>
2015-05-03 09:33:16 -0700

    Part 23 - How to Make Your Enemies Fire Projectiles

---
+++ Assets/Scripts/EnemyStarController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyStarController : MonoBehaviour
+{
+	public float speed;
+	public PlayerController player;
+	public GameObject impactEffect;
+	public float rotationSpeed;
+	public int damageToGive;
+	
+	private Rigidbody2D rigidbody2D;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		this.rigidbody2D = GetComponent<Rigidbody2D> ();
+		this.player = FindObjectOfType<PlayerController> ();
+		
+		if (this.player.transform.position.x < transform.position.x) {
+			this.speed = -this.speed;
+			this.rotationSpeed = -this.rotationSpeed;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		this.rigidbody2D.velocity = new Vector2 (this.speed, this.rigidbody2D.velocity.y);
+		this.rigidbody2D.angularVelocity = this.rotationSpeed;
+	}
+	
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.name == "Player") {
+			HealthManager.HurtPlayer(this.damageToGive);
+		}
+		
+		Instantiate (this.impactEffect, transform.position, transform.rotation);
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/ShootAtPlayerInRange.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootAtPlayerInRange : MonoBehaviour
+{
+	public float playerRange;
+	public GameObject enemyStar;
+	public PlayerController player;
+	public Transform launchPoint;
+	public float waitBetweenShots;
+
+	private float shotCounter;
+
+	// Use this for initialization
+	void Start ()
+	{
+		this.player = FindObjectOfType<PlayerController> ();
+		this.shotCounter = this.waitBetweenShots;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Debug.DrawLine (new Vector3 (transform.position.x - this.playerRange, transform.position.y, transform.position.z), new Vector3 (transform.position.x + this.playerRange, transform.position.y, transform.position.z));
+		this.shotCounter -= Time.deltaTime;
+
+		if (this.shotCounter < 0) {
+			if (transform.localScale.x < 0 && this.player.transform.position.x > transform.position.x && this.player.transform.position.x < transform.position.x + this.playerRange) {
+				this.Shoot ();
+			}
+
+			if (transform.localScale.x > 0 && this.player.transform.position.x < transform.position.x && this.player.transform.position.x > transform.position.x - this.playerRange) {
+				this.Shoot ();
+			}
+		}
+	}
+
+	void Shoot ()
+	{
+		Instantiate (this.enemyStar, this.launchPoint.position, this.launchPoint.rotation);
+		this.shotCounter = this.waitBetweenShots;
+	}
+}
